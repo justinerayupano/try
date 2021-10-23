5a91792285255326151996481d225c6acae7ffda
Justine <ujusine@yahoo.com>
2021-10-23 17:58:21 +0800

    first commit

---
+++ 2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int intA = 10000;
+    int input;
+    int result;
+
+    cout << "Type a number: ";
+    cin >> input;
+
+    result = intA - input;
+    cout << result;
+  return 0;
+}
